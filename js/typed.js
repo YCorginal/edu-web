5f4cc1b8bd5ab9e786ae933b60c6d60da1e446ee
YCorginal <94970658+YCorginal@users.noreply.github.com>
2022-03-26 23:24:39 +0600

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typed', {
+    // Waits 1000ms after typing "First"
+    strings: ['Learn Like a Pro',
+     'Share Your Ideas To All', 
+     'Your Knowledge Is Your Special Ability', 
+     'Your Imagination Power Is More Then You Think'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+});  
